ff1ba780b12b6a124b043f06fcc7a94cdc80fda0
luxy17 <damnlux21@gmail.com>
2022-03-05 09:08:10 -0800

    first commit

---
+++ index.php
@@ -0,0 +1,346 @@
+<!doctype html>
+<html lang="en">
+  <head>
+    <!-- Required meta tags -->
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <link rel="preconnect" href="https://fonts.googleapis.com">
+    <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
+    <link href="https://fonts.googleapis.com/css2?family=Poppins:ital,wght@0,200;1,600&display=swap" rel="stylesheet">
+    <!-- Bootstrap CSS -->
+    <link href="assets/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-1BmE4kWBq78iYhFldvKuhfTAU6auU8tT94WrHftjDbrCEXSU1oBoqyl2QvZ6jIW3" crossorigin="anonymous">
+
+    <title>Computer Education</title>
+  </head>
+  <body>
+      <!-- navbar -->
+      <!-- d-flex align-items-center justify-content-between flex-wrap position-relative -->
+  <nav class="navbar fixed-top navbar-expand-lg navbar-white bg-white shadow-sm" >
+  <div class="container-fluid ">
+    <div class="px-3">
+        <a class="navbar-brand" href="#">
+        <img src="img/logo_ce.png" alt="" width="120" height="40" class="d-inline-block align-text-top">
+        </a>
+    </div>
+    <button class="navbar-toggler bg-light" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
+      <span class="navbar-toggler-icon"></span>
+    </button>
+    <div class="collapse navbar-collapse "  id="navbarNav">
+      <ul class="navbar-nav m-auto " >
+        <div class=" px-1">
+            <li class="nav-item">
+            <a class="nav-link" style="color: #348A0C;" href="#">Sejarah</a>
+            </li>
+        </div>
+
+        <div class="  px-1">
+            <li class="nav-item">
+            <a class="nav-link" style="color: #348A0C;" href="#">Divisi</a>
+            </li>
+        </div>
+
+        <div class=" px-1">
+            <li class="nav-item">
+            <a class="nav-link" style="color: #348A0C;" href="#">Pengurus</a>
+            </li>
+        </div>
+
+        <div class=" px-1">
+            <li class="nav-item">
+            <a class="nav-link" style="color: #348A0C;"  href="#">Galeri</a>
+            </li>
+        </div>
+
+        <div class="px-1">
+        <li class="nav-item ">
+          <a class="nav-link" style="color: #348A0C;" href="#">Artikel</a>
+        </li>
+        </div>
+
+        <a class="btn btn-outline-success " href="masuk.php"type="submit">Masuk</a>
+   
+      </ul> 
+
+    </div>
+  </div>
+</nav>
+
+ <!-- banner-navbar -->
+<div  style="height: 720px;">
+<br>
+<br>
+<div class="row mt-5 align-items-center">
+    <div class="col m-2">
+    <p style="font-family : Poppins; font-size: 30px; color: #348A0C; ">
+    Unit Kegiatan Mahasiswa
+    </p>
+    <p style="font-family : Poppins; font-size: 30px; color: #348A0C; ">
+    Computer Education
+    </p>
+    <br>
+
+    <p style="font-family: 'Poppins', sans-serif; font-size: 15px;  ">
+    Computer education adalah unit kegiatan mahasiswa
+    yang berguna untuk menampung wadah mahasiswa
+    khususnya dalam bidang IT dan Tekhnologi.
+    </p>
+    
+    </div>
+    <div class="col">
+    <img src="img/animasi.png" width="500" height="400">
+    </div>
+
+</div>
+</div >
+<br>
+
+
+
+<div class ="bg-light"style="height: 600px;">
+<p class= "m-2" style="font-family : Poppins; font-size: 30px; color: #348A0C; ">
+ Sejarah
+</p>
+<div class="row mt-5 ">
+    <div class="col m-2">
+    <p style="font-family: 'Poppins', sans-serif; font-size: 15px;  ">
+    Lorem ipsum dolor sit amet consectetur adipisicing elit. Quas incidunt, sunt nobis impedit non aperiam distinctio, facere quisquam nisi illo harum dolorem architecto eaque doloribus aut. Cum impedit ipsum ratione.
+    Lorem ipsum dolor sit amet consectetur adipisicing elit. Natus cumque illum facilis alias sed, unde neque nostrum tempore minima quisquam voluptatibus! Ab qui earum natus cupiditate tenetur deserunt blanditiis dolores?
+    Lorem, ipsum dolor sit amet consectetur adipisicing elit. Reiciendis tempore doloremque possimus ullam? Sunt praesentium voluptas provident, autem magni facere ut rem ipsam magnam maxime alias aliquam quas dolores a.
+
+    </p>    
+    </p>
+    
+    </div>
+    <div class="col">
+    <span><img src="img/animasi.png" width="400" height="120" class="img-circle" style="item-align: center;">  </span>
+    <p style="font-family: 'Poppins', sans-serif; font-size: 15px; text-align: center; ">
+    nama : Riskiyudin <br>
+    prodi :  teknik informatika<br>
+    periode : 2001-2002
+    </p>
+    </div>
+
+</div>
+</div>
+
+<div style="height: 720px;">
+<p class= "m-2" style="font-family : Poppins; font-size: 30px; color: #348A0C; ">
+ Divisi
+</p>
+<p class= "m-2">
+    Lorem ipsum dolor sit amet, consectetur adipisicing elit. Hic obcaecati in accusamus? Unde quidem laudantium enim nobis adipisci, ullam alias saepe commodi, maxime accusamus reprehenderit labore, totam nesciunt dolore eveniet?
+</p>
+
+<div class="row">
+  <div class="col-sm-6">
+    <div class="card">
+      <div class="card-body">
+        <h5 class="card-title">Divisi Organisasi</h5>
+        <p class="card-text">Lorem ipsum, dolor sit amet consectetur adipisicing elit. Pariatur repellendus quidem error autem tempore suscipit dignissimos nisi hic quas asperiores, repudiandae ipsum, corporis omnis necessitatibus? Omnis, nulla cumque! Quam, temporibus?
+        Lorem ipsum, dolor sit amet consectetur adipisicing elit. Tempore molestias quis soluta. Doloribus rem harum laudantium assumenda dolor, sunt officiis eius id a voluptate quia nostrum dignissimos accusamus quidem fuga!
+        </p>
+
+      </div>
+    </div>
+  </div>
+  <div class="col-sm-6">
+    <div class="card">
+      <div class="card-body">
+        <h5 class="card-title">Divisi Komputer</h5>
+        <p class="card-text">Lorem ipsum, dolor sit amet consectetur adipisicing elit. Pariatur repellendus quidem error autem tempore suscipit dignissimos nisi hic quas asperiores, repudiandae ipsum, corporis omnis necessitatibus? Omnis, nulla cumque! Quam, temporibus?
+        Lorem ipsum, dolor sit amet consectetur adipisicing elit. Tempore molestias quis soluta. Doloribus rem harum laudantium assumenda dolor, sunt officiis eius id a voluptate quia nostrum dignissimos accusamus quidem fuga!</p>
+      </div>
+    </div>
+  </div>
+</div>
+
+</div>
+
+
+<div class ="bg-light"style="height: 1200px;">
+<p class= "m-2" style="font-family : Poppins; font-size: 30px; color: #348A0C; ">
+Pengurus
+</p>
+<p class= "m-2 text-center">
+    BPH Periode 2021-2022
+</p>
+<span class=    ><img src="img/animasi.png" width="400" height="120" class="img-circle" style="item-align: center;">
+<div class="row align-items-end">
+    
+    <div class="col">
+    <span><img src="img/animasi.png" width="400" height="120" class="img-circle" style="item-align: center;">
+    </div>
+    <div class="col">
+    <span><img src="img/animasi.png" width="400" height="120" class="img-circle" style="item-align: center;">
+    </div>
+    <div class="col">
+    <span><img src="img/animasi.png" width="400" height="120" class="img-circle" style="item-align: center;">
+    </div>
+  </div>
+
+  </div >
+
+
+  <div  style="height: 700px;">
+  <p class= "m-2" style="font-family : Poppins; font-size: 30px; color: #348A0C; ">
+    Galeri
+  </p>
+    <div id="carouselExampleControls" class="carousel slide" data-ride="carousel">
+    <div class="carousel-inner">
+        <div class="carousel-item active">
+        <img class="d-block w-50" src="img/animasi.png" alt="First slide">
+        </div>
+        <div class="carousel-item">
+        <img class="d-block w-50" src="img/animasi.png" alt="Second slide">
+        </div>
+        <div class="carousel-item">
+        <img class="d-block w-50" src="img/animasi.png" alt="Third slide">
+        </div>
+    </div>
+    <a class="carousel-control-prev" href="#carouselExampleControls" role="button" data-slide="prev">
+        <span class="carousel-control-prev-icon" aria-hidden="true"></span>
+        <span class="sr-only">Previous</span>
+    </a>
+    <a class="carousel-control-next" href="#carouselExampleControls" role="button" data-slide="next">
+        <span class="carousel-control-next-icon" aria-hidden="true"></span>
+        <span class="sr-only">Next</span>
+    </a>
+    </div>
+</div>
+
+    <div class ="bg-light" style="height: 1400px;">
+    <p class= "m-2" style="font-family : Poppins; font-size: 30px; color: #348A0C; ">
+    Artikel
+    </p>
+  <div class="row row-cols-1 row-cols-md-3 g-4 m-2">
+  <div class="col">
+    <div class="card">
+      <img src="..." class="card-img-top" alt="...">
+      <div class="card-body">
+        <h5 class="card-title">Card title</h5>
+        <p class="card-text">This is a longer card with supporting text below as a natural lead-in to additional content. This content is a little bit longer.</p>
+        <a href="#" class="card-link" style="color: #348A0C;" >Baca Selengkapnya</a>
+      </div>
+      
+    </div>
+  </div>
+  <div class="col">
+    <div class="card">
+      <img src="..." class="card-img-top" alt="...">
+      <div class="card-body">
+        <h5 class="card-title">Card title</h5>
+        <p class="card-text">This is a longer card with supporting text below as a natural lead-in to additional content. This content is a little bit longer.</p>
+        <a href="#" class="card-link" style="color: #348A0C;" >Baca Selengkapnya</a>
+      </div>
+    </div>
+  </div>
+  <div class="col">
+    <div class="card">
+      <img src="..." class="card-img-top" alt="...">
+      <div class="card-body">
+        <h5 class="card-title">Card title</h5>
+        <p class="card-text">This is a longer card with supporting text below as a natural lead-in to additional content.</p>
+        <a href="#" class="card-link" style="color: #348A0C;" >Baca Selengkapnya</a>
+      </div>
+    </div>
+  </div>
+  <div class="col">
+    <div class="card">
+      <img src="..." class="card-img-top" alt="...">
+      <div class="card-body">
+        <h5 class="card-title">Card title</h5>
+        <p class="card-text">This is a longer card with supporting text below as a natural lead-in to additional content. This content is a little bit longer.</p>
+        <a href="#" class="card-link" style="color: #348A0C;" >Baca Selengkapnya</a>
+      </div>
+    </div>
+  </div>
+  <div class="col">
+    <div class="card">
+      <img src="..." class="card-img-top" alt="...">
+      <div class="card-body">
+        <h5 class="card-title">Card title</h5>
+        <p class="card-text">This is a longer card with supporting text below as a natural lead-in to additional content. This content is a little bit longer.</p>
+        <a href="#" class="card-link" style="color: #348A0C;" >Baca Selengkapnya</a>
+      </div>
+    </div>
+  </div>
+  <div class="col">
+    <div class="card">
+      <img src="..." class="card-img-top" alt="...">
+      <div class="card-body">
+        <h5 class="card-title">Card title</h5>
+        <p class="card-text">This is a longer card with supporting text below as a natural lead-in to additional content. This content is a little bit longer.</p>
+        <a href="#" class="card-link" style="color: #348A0C;" >Baca Selengkapnya</a>
+      </div>
+    </div>
+  </div>
+</div>
+    </div>  
+</div>
+</div>
+
+
+
+<!-- footer -->
+<div class="container"> 
+  <footer class="py-5" >
+    <div class="row">
+      <div class="col-5">
+      <p style="font-family : Poppins; font-size: 22px; color: ; ">
+      Unit Kegiatan Mahasiswa<br>
+      Universitas Muhammadiyah Cirebon<br>
+      Computer Education
+      </p>
+      </div>
+
+      <div class="col-2">
+        <ul class="nav flex-column">
+          <li class="nav-item mb-2"><a href="#" class="nav-link p-0 text-muted">Artikel</a></li>
+          <li class="nav-item mb-2"><a href="#" class="nav-link p-0 text-muted">Sejarah</a></li>
+          <li class="nav-item mb-2"><a href="#" class="nav-link p-0 text-muted">Galeri</a></li>
+        </ul>
+      </div>
+
+      <div class="col-2">
+        <ul class="nav flex-column">
+          <li class="nav-item mb-2"><a href="#" class="nav-link p-0 text-muted">Pengurus</a></li>
+          <li class="nav-item mb-2"><a href="#" class="nav-link p-0 text-muted">Divisi</a></li>
+        </ul>
+      </div>
+
+      <div class="col-2 ">
+        <form>
+          <h5>Social Media</h5>
+          <p>@computerws</p>
+          <p>@computerws</p>
+          <div class="d-flex w-100 gap-2">
+ 
+  
+          </div>
+        </form>
+      </div>
+    </div>
+
+    <div class="d-flex justify-content-between py-4 my-4 border-top">
+      <p>© 2022 Computer Education</p>
+      <ul class="list-unstyled d-flex">
+        <li class="ms-3"><a class="link-dark" href="#"><svg class="bi" width="24" height="24"><use xlink:href="#twitter"></use></svg></a></li>
+        <li class="ms-3"><a class="link-dark" href="#"><svg class="bi" width="24" height="24"><use xlink:href="#instagram"></use></svg></a></li>
+        <li class="ms-3"><a class="link-dark" href="#"><svg class="bi" width="24" height="24"><use xlink:href="#facebook"></use></svg></a></li>
+      </ul>
+    </div>
+  </footer>
+</div>
+
+    <!-- Optional JavaScript; choose one of the two! -->
+
+    <!-- Option 1: Bootstrap Bundle with Popper -->
+    <script src="assets/js/bootstrap.bundle.min.js" integrity="sha384-ka7Sk0Gln4gmtz2MlQnikT1wXgYsOg+OMhuP+IlRH9sENBO0LRn5q+8nbTov4+1p" crossorigin="anonymous"></script>
+
+    <!-- Option 2: Separate Popper and Bootstrap JS -->
+    <!--
+    <script src="https://cdn.jsdelivr.net/npm/@popperjs/core@2.10.2/dist/umd/popper.min.js" integrity="sha384-7+zCNj/IqJ95wo16oMtfsKbZ9ccEh31eOz1HGyDuCQ6wgnyJNSYdrPa03rtR1zdB" crossorigin="anonymous"></script>
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/js/bootstrap.min.js" integrity="sha384-QJHtvGhmr9XOIpI6YVutG+2QOK9T+ZnN4kzFN1RtK3zEFEIsxhlmWl5/YESvpZ13" crossorigin="anonymous"></script>
+    -->
+  </body>
+</html>
